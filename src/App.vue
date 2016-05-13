3f0dd8c1593b5d4829b4c1bdcc60bdab1ae1d82e
lxz <lxz318@aliyun.com>
2016-05-13 18:13:27 +0800

    add background for visitors.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <app-visitor v-if="!isAuthenticated"></app-visitor>
   <app-wrapper v-if="isAuthenticated"></app-wrapper>
 
   <app-modal></app-modal>
 </template>
 <script type="text/babel">
+  import AppVisitor from './components/AppVisitor';
   import AppWrapper from './components/AppWrapper';
   import AppModal from './components/AppModal';
 
@@ -16,6 +18,7 @@
       }
     },
     components: {
+      AppVisitor,
       AppWrapper,
       AppModal
     }
---
+++ src/components/AppVisitor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grey lighten-1">
+    <div class="row">
+      <h1>Hello, You are not signed in!!</h1>
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+
+  export default{}
+</script>
+
+<style>
+
+</style>
