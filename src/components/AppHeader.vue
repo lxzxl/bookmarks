e339092ee35474291a16f2116da4afb3daacd688
steven <sli@movoto.com>
2016-05-19 21:42:31 +0800

    fix header component.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,21 @@
 <template>
-  <nav>
-    <nav class="black header" role="navigation">
-      <div class="nav-wrapper container">
-        <a id="logo-container" href="#" class="brand-logo">Bookmarks</a>
-        <ul class="right">
-          <li><a href="#" @click="doSignOut">Sign Out</a></li>
-        </ul>
+  <nav class="black header" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">Bookmarks</a>
+      <ul class="right">
+        <li>
+          <a href="#" @click="doSignOut">
+            <i class="material-icons right">exit_to_app</i>
+            <span class="hide-on-med-and-down">Sign Out</span>
+          </a>
+        </li>
+      </ul>
 
-        <!--<ul id="nav-mobile" class="side-nav" style="transform: translateX(-100%);">-->
-        <!--<li><a href="#">Navbar Link</a></li>-->
-        <!--</ul>-->
-        <!--<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>-->
-      </div>
-    </nav>
+      <!--<ul id="nav-mobile" class="side-nav" style="transform: translateX(-100%);">-->
+      <!--<li><a href="#">Navbar Link</a></li>-->
+      <!--</ul>-->
+      <!--<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>-->
+    </div>
   </nav>
 </template>
 
