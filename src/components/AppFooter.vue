691429f86c88466b07dcba400147ee86c7050fbe
steven <sli@movoto.com>
2016-05-20 21:39:14 +0800

    remove debug code.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,17 +12,7 @@
 </template>
 
 <script type="text/babel">
-  export default {
-    data () {
-      return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello Footer!'
-      }
-    }
-  }
+  export default {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
